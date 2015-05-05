1c459b6d63590f0e43bcd01b2d76e77717972bc1
Etienne Dublé <etienne.duble@imag.fr>
2015-05-05 13:21:12 +0200

    More config items for rpi debian kernel.

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 DOCKER_IMAGE_MAINTAINER="Etienne Duble <etienne.duble@imag.fr>"
 DOCKER_USER="waltplatform"
 
-DEBIAN_VERSION="wheezy"
+DEBIAN_VERSION="jessie"
 DEBIAN_ARCHIVE_GPG_KEY="8B48AD6246925553"
 
 DOCKER_DEV_MASTER_IMAGE="$DOCKER_USER/dev-master"
@@ -23,6 +23,39 @@ DEBIAN_RPI_REPO_URL="http://mirror.switch.ch/ftp/mirror/raspbian/raspbian"
 DEBIAN_RPI_REPO_KEY="http://mirror.switch.ch/ftp/mirror/raspbian/raspbian.public.key"
 DEBIAN_RPI_REPO_VERSION="wheezy"
 DEBIAN_RPI_REPO_SECTIONS="main contrib non-free rpi"
+DEBIAN_RPI_KERNEL_REPO="git://github.com/raspberrypi/linux.git"
+DEBIAN_RPI_KERNEL_BRANCH_NAME="rpi-3.12.y"
+DEBIAN_RPI_KERNEL_COMMIT="15d7e6373f54b99fce2ba5375b75cc11549af751"
+# the overlayfs module is needed for the union of filesystems
+# (manage the many nodes that may want to write to the same
+# exported filesystem; they will write in memory instead)
+# the branch is linked to a specific linux kernel
+# version (check in the git log).
+# From kernel 3.18 onwards, overlayfs is merged in mainline,
+# which should simplify this step.
+# However, there are issues with kexec when using this new
+# kernel versions. (as of may 5, 2015)
+DEBIAN_RPI_KERNEL_OVERLAYFS_REPO="https://kernel.googlesource.com/pub/scm/linux/kernel/git/mszeredi/vfs"
+DEBIAN_RPI_KERNEL_OVERLAYFS_BRANCH_NAME="overlayfs.v20"
+
+# the firmware repository contains the SoC firmware
+# that is loaded on the board startup.
+# the firmware is linked to a specific linux kernel
+# version (check in the git log)
+DEBIAN_RPI_FIRMWARE_VERSION="09c0f5fc8cd9701f01e8e403c398699e1b1a1eb6"
+
+# kernel upgrade notes:
+# we can retrieve raspbian's running kernel config
+# by issuing
+# $ zcat /proc/config.gz > /raspbian_config
+# then this file can be copied in the kernel compilation
+# directory, renamed '.config'.
+# After that we issue:
+# $ make olddefconfig
+# that will set all new config options missing in the existing
+# .config file, to their default value.
+# Some other options may be changed if needed (see wiki),
+# using the menu interface ('make nconfig' or similar).
 
 HOST_FS_PATH="/host_fs"
 RPI_FS_PATH="/rpi_fs"
